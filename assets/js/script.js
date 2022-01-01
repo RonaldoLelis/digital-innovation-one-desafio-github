7e41acf77b2e711ab535e21497fa97c2258f4e65
Ronaldo Lelis <ronaldo.lelis@perinity.com>
2022-01-01 16:41:43 -0300

    alterando as classes e completando o desafio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
-let currentNumberWrapper = document.getElementById("currentNumber");
-let currentNumber = 0;
+// iniciar o contador com zero
+let count = 0;
 
-function increment() {
-    currentNumber++;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
-function decrement() {
-    currentNumber--;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+// selecionar o valor e o botão clicado
+const VALUE = document.querySelector('#currentNumber');
+const BTNS = document.querySelectorAll('.btn');
+
+// loop para mudar o valor e setar as classes
+BTNS.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const STYLES = e.currentTarget.classList;
+        if(STYLES.contains('decrement')) {
+            count--;
+        }
+        else if(STYLES.contains('increment')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if(count > 0) {
+            VALUE.style.color = 'green';
+        }
+        if(count < 0) {
+            VALUE.style.color = 'red';            
+        }
+        if(count === 0) {
+            VALUE.style.color = '#222'
+        }
+        VALUE.textContent = count;
+    });
+});
+
+
+
